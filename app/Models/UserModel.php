ad711515b6a7e01d387d72e1e58ba4dc3e5e1961
jozhua <joshuazabala693@gmail.com>
2025-09-09 22:56:06 +0800

    model for user

---
+++ app/Models/UserModel.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class UserModel extends Model
+{
+    protected $table            = 'user';
+    protected $primaryKey       = 'id';
+    protected $allowedFields    = [
+        'firstname', 
+        'midlename',
+        'lastname',
+        'address',
+        'contact_number',
+        'username',
+        'email',
+        'password'
+    ];
+
+    protected $beforeInsert = ['hashPassword'];
+    protected $beforeUpdate = ['hashPassword'];
+
+    protected function hashPassword(array $data)
+    {
+        if (isset($data['data']['password'])) {
+            $data['data']['password'] = password_hash($data['data']['password'], PASSWORD_BCRYPT);
+        }
+        return $data;
+    }
+}
