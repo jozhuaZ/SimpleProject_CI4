999e8619d09b713168605fb2c98789e2bc73741d
jozhua <joshuazabala693@gmail.com>
2025-09-11 23:31:50 +0800

    New controller for admin. has userRecord

---
+++ app/Controllers/AdminController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\UserModel;
+
+class AdminController extends BaseController
+{
+    public function index()
+    {
+        // return view('Admin/Dashboard/index');
+    }
+
+    public function userRecord()
+    {
+        try {
+            $userModel = new UserModel();
+            $users = $userModel->fetchUsers();
+
+            $data['users'] = $users;
+        } catch (\Throwable $e) {
+            log_message('error', $e->getMessage());
+            $data['users'] = [];
+        }
+
+        return view('Admin/User/userRecord', $data);
+    }
+}
